25bf7c43cee64e798a8965666f8dd9682da239d5
Puneet53-84 <puneet.sharma8453@gmail.com>
2025-08-18 13:02:10 +0530

    api of todo is done

--- day 14/backend-bank-app/db.js
+++ day 14/backend-bank-app/db.js
@@ -1,27 +1,27 @@
 import { MongoClient } from 'mongodb'
 
-let db = null;
+let isDbConnected = null;
 
 const CLUSTER_URL = "mongodb+srv://puneetsharmait5418:1eE617EeJ8ykP8gN@cluster0.n5dmm4v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(CLUSTER_URL)
 
 //async make a function asynchronous,we can use await
 
-async function connectToDatabase(dbName) {
-    if (db == null) {
+async function connectToDatabase(isDbConnected) {
+    if (isDbConnected == null) {
         try{
 
         //await waits for a mongodb connection to finish before moving to the next line in an async function.
-        db = await client.connect(dbName);
-        db=client.db(dbName)
+        isDbConnected = await client.connect(dbName);
+        isDbConnected=client.db(dbName)
         console.log("database connected succesfull")
         } catch(e){
             console.log(`database not connected,error ${e.message}`)
         }
     } else {
-        console.log("database not connected}")
+        console.log("database already connected}")
         
     }
-    return db;
+    return isDbConnected;
 }
 export default connectToDatabase
--- day 14/backend-bank-app/server.js
+++ day 14/backend-bank-app/server.js
@@ -5,12 +5,13 @@ import connectToDatabase from './db.js'
 const app = express()
 // define port 
 const port = 3000
+app.use(express.json())
 
-
+let db;
 // start server [node server.js]
 app.listen(port,async() => {
     console.log(`server is started at ${port}`)
-    await connectToDatabase('bank-db')
+    db=await connectToDatabase('bank-db')
 })
 
 
@@ -22,4 +23,36 @@ api section
 app.post('/test-api', (req, res) => {
     
     res.send("api is up")
+})
+
+
+/*
+API 1: Insert Account in Database
+
+http method -> post
+url -> '/create-account'
+request -> body {
+            'accountNo': '123ABC',
+            'userName': 'dheeraj',
+            'userNo': 12312213
+        }
+response - status = 201 body = {
+                            'msg': 'account inserted successfully
+                        }
+*/
+
+app.post('/create-account', async (req,res) =>{
+    try{
+        let userAccountData=req.body;
+        console.log(`request body ${JSON.stringify(userAccountData)}`)
+        await db.collection(`account`).insertOne(userAccountData);
+        res.status(201).json({
+            msg:'account inserted successfully'
+        })
+    }
+    catch(error){
+        res.status(500).send({
+            error:error.message
+        })
+    }
 })
